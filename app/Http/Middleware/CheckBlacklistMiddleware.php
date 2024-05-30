97ba0101814587c1d5ef97c459d17617812aabb9
HamzaNisar222 <hamzanisar941@gmail.com>
2024-05-30 18:26:40 +0500

    2nd commit

--- app/Http/Middleware/CheckBlacklistMiddleware.php
+++ app/Http/Middleware/CheckBlacklistMiddleware.php
@@ -5,32 +5,35 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use App\Models\ActiveToken;
 use App\Models\BlacklistedToken;
 
 class CheckBlacklistMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
     public function handle(Request $request, Closure $next)
     {
-        // Retrieve the token from the request
-        $token = JWTAuth::getToken();
+        try {
+            // Retrieve the token from the request
+            $token = JWTAuth::getToken();
 
-        // Check if the token is not present
-        if (!$token) {
-            return response()->json(['error' => 'Token not provided'], 400);
+            if (!$token) {
+                return response()->json(['error' => 'Token not provided'], 400);
+            }
+            if (BlacklistedToken::where('token', $token)->exists()) {
+                return response()->json(['error' => 'Token is blacklisted'], 401);
+            }
+
+            return $next($request);
+        } catch (TokenExpiredException $e) {
+            
+            $token = JWTAuth::getToken();
+            if ($token) {
+                ActiveToken::where('token', $token)->delete();
+            }
+            return response()->json(['error' => 'Token has expired'], 401);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Token validation failed'], 401);
         }
-
-        // Check if the token is blacklisted
-        if (BlacklistedToken::where('token', $token)->exists()) {
-            return response()->json(['error' => 'Token is blacklisted'], 401);
-        }
-
-        return $next($request);
     }
 }
